6c28613fee51bd27db088a2f019bade320573f96
fxcoder <barabass@gmail.com>
2015-11-11 22:50:47 +0200

    new config

--- picasa-to-blog/src/main/java/com/axisdesktop/picasatoblog/config/AppInitializer.java
+++ picasa-to-blog/src/main/java/com/axisdesktop/picasatoblog/config/AppInitializer.java
@@ -1,23 +1,20 @@
 package com.axisdesktop.picasatoblog.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-public class AppInitializer implements WebApplicationInitializer {
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { AppConfig.class, PersistenceConfig.class };
+	}
 
 	@Override
-	public void onStartup( ServletContext container ) throws ServletException {
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.register( AppConfig.class, PersistenceConfig.class );
-		ctx.setServletContext( container );
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { MvcConfig.class };
+	}
 
-		ServletRegistration.Dynamic servlet = container.addServlet( "dispatcher", new DispatcherServlet( ctx ) );
-		servlet.setLoadOnStartup( 1 );
-		servlet.addMapping( "/" );
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
 	}
 }
